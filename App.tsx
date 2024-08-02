1df80376049149cb15c33dc6ebe3e8bf87ed9e41
Sarah Markless <sarah.markless2@sky.uk>
2024-08-02 16:03:59 +0100

    WIP

--- App.tsx
+++ App.tsx
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import Animated, {
+  Easing,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+
+import {
+  GestureHandlerRootView,
+  GestureDetector,
+  Gesture,
+} from "react-native-gesture-handler";
+import { useCallback } from "react";
+
+const { height, width } = Dimensions.get("window");
+const initialHeight = height / 2;
 
 export default function App() {
+  // const onTap = useCallback(() => Gesture.Tap(), []);
+  const onHold = useCallback(() => Gesture.LongPress(), []);
+
+  const animatedBackgroundHeight = useSharedValue(initialHeight);
+  const onTap = useCallback(() => {
+    animatedBackgroundHeight.value = withTiming(0, {
+      duration: 1000,
+      easing: Easing.out(Easing.exp),
+    });
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView>
+      <View style={styles.container}>
+        <Animated.View
+          style={[styles.topHalf, { height: animatedBackgroundHeight }]}
+        />
+        <Animated.View
+          style={[styles.bottomHalf, { height: animatedBackgroundHeight }]}
+        />
+        {/* <GestureDetector gesture={Gesture.Exclusive(onTap(), onHold())}> */}
+        <Animated.View style={[styles.circle]} onTouchStart={onTap}>
+          <Text>Welcome</Text>
+        </Animated.View>
+        {/* </GestureDetector> */}
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  topHalf: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "red",
+  },
+  bottomHalf: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "deeppink",
+  },
+  circle: {
+    height: width * 0.7,
+    width: width * 0.7,
+    backgroundColor: "orange",
+    borderRadius: width * 0.7,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
